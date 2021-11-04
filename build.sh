6a31ae81d8332653de307ca0bdcc82f6389327ec
oladimeji idowu <oladimeji.idowu001@gmail.com>
2021-11-04 16:55:37 -0400

    merged all files in bash

--- build.sh
+++ build.sh
@@ -1,1 +1,4 @@
 cat templates/top.html content/index.html templates/bottom.html > docs/index.html
+cat templates/top.html content/project.html templates/bottom.html > docs/project.html
+cat templates/top.html content/blog.html templates/bottom.html > docs/blog.html
+cat templates/top.html content/contact.html templates/bottom.html > docs/contact.html
