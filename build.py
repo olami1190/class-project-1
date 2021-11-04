141054158ffdb9d267f238f3bde2e6af64ecb66a
oladimeji idowu <oladimeji.idowu001@gmail.com>
2021-11-04 17:17:06 -0400

    added combined project file to docs dir

--- build.py
+++ build.py
@@ -2,4 +2,10 @@ top_html = open('./templates/top.html') .read()
 bottom_html = open('./templates/bottom.html') .read()
 middle_html = open('./content/index.html') .read()
 combined= top_html + middle_html + bottom_html
+
 open ('docs/index.html', 'w+') .write(combined)
+
+
+middle_html = open('./content/project.html') .read()
+combined= top_html + middle_html + bottom_html
+open ('docs/project.html', 'w+') .write(combined)
