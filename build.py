5f492f8cfcc4960a76a0fcc6b782b39f5c925707
oladimeji idowu <oladimeji.idowu001@gmail.com>
2021-11-04 17:18:24 -0400

    added combined blog file to docs dir

--- build.py
+++ build.py
@@ -9,3 +9,9 @@ open ('docs/index.html', 'w+') .write(combined)
 middle_html = open('./content/project.html') .read()
 combined= top_html + middle_html + bottom_html
 open ('docs/project.html', 'w+') .write(combined)
+
+
+middle_html = open('./content/blog.html') .read()
+combined= top_html + middle_html + bottom_html
+
+open ('docs/blog.html', 'w+') .write(combined)
